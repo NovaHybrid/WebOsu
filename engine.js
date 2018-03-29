298c0ec9ad293015d3c73cf024abaccd394ba0b2
BenJilks <benjyjilks@gmail.com>
2018-03-29 19:55:32 +0000

    Init

---
+++ engine.js
@@ -0,0 +1,374 @@
+
+// Define constants
+const fps = 30;
+var scale = 0.7;
+var osuAsp = 512.0 / 384.0;
+var asp;
+
+// Define quad data
+var quad_vertices = [
+    -0.5,0.5,0.0,
+    -0.5,-0.5,0.0,
+    0.5,-0.5,0.0, 
+    0.5,0.5,0.0
+];
+var quad_textures = [
+    0, 0,
+    0, 1,
+    1, 1,
+    1, 0
+];
+var quad_indices = [
+    0,1,2, 
+    2,3,0
+];
+
+var gl, display;
+var quad, shader;
+var texture_load = 0;
+
+function initGameEngine() 
+{
+	display = document.getElementById("display");
+	gl = display.getContext("webgl2");
+    gl.enable(gl.BLEND);
+	gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, 
+		gl.ONE, gl.ONE_MINUS_SRC_ALPHA);   
+    
+    quad = loadModel(quad_vertices, quad_textures, quad_indices);
+	var vertCode = document.getElementById("vert").text;
+	var fragCode = document.getElementById("frag").text;
+    shader = loadShader(vertCode, fragCode);
+}
+
+function loadModel(vertices, textures, indices) 
+{
+	var vao = gl.createVertexArray();
+	gl.bindVertexArray(vao);
+	
+	var vertex_buffer = loadBuffer(vertices, 0, 3);
+    var texture_buffer = loadBuffer(textures, 1, 2);
+	var index_buffer = loadIndex(indices);
+	gl.bindVertexArray(null);
+	return {vao: vao, length: indices.length};
+}
+
+function loadBuffer(data, index, unit_size) 
+{
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(index, unit_size, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(index);
+	return buffer;
+}
+
+function loadIndex(data) 
+{
+	var index = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), gl.STATIC_DRAW);
+    return index;
+}
+
+function genLineMesh(p1, p2, rad) 
+{
+    var diff = vec2.create(), 
+        dir = vec2.create(),
+        offset = vec2.create(),
+        ends = vec2.create();
+    vec2.sub(diff, p2, p1);
+	vec2.normalize(dir, diff);
+    var invDir = [dir[1], dir[0]];
+    
+    vec2.multiply(offset, invDir, [rad/1.5, rad/1.5]);
+    vec2.multiply(ends, dir, [rad/1.5, rad/1.5]);
+    var positions = [
+        // Start
+        p1[0]-offset[0]+ends[0], p1[1]+offset[1]+ends[1], 0.0,
+        p1[0]+offset[0]+ends[0], p1[1]-offset[1]+ends[1], 0.0,
+        p1[0]+offset[0], p1[1]-offset[1], 0.0,
+        p1[0]-offset[0], p1[1]+offset[1], 0.0,
+        
+        // End
+        p2[0]-offset[0]-ends[0], p2[1]+offset[1]-ends[1], 0.0,
+        p2[0]+offset[0]-ends[0], p2[1]-offset[1]-ends[1], 0.0,
+        p2[0]+offset[0], p2[1]-offset[1], 0.0,
+        p2[0]-offset[0], p2[1]+offset[1], 0.0,
+
+    	// Body
+        p1[0]-offset[0]+ends[0], p1[1]+offset[1]+ends[1], 0.0,
+        p1[0]+offset[0]+ends[0], p1[1]-offset[1]+ends[1], 0.0,
+        p2[0]+offset[0]-ends[0], p2[1]-offset[1]-ends[1], 0.0,
+        p2[0]-offset[0]-ends[0], p2[1]+offset[1]-ends[1], 0.0,
+    ];
+    var textures = [
+        // Start
+		0.5, 0.0,
+		0.5, 1.0,
+		0.0, 1.0,
+		0.0, 0.0,
+		
+        // End
+		0.5, 0.0,
+		0.5, 1.0,
+		0.0, 1.0,
+		0.0, 0.0,
+        
+        // Body
+		0.5, 0.0,
+		0.5, 1.0,
+		0.7, 1.0,
+		0.7, 0.0,
+    ];
+    var index = [
+    	0,1,2, 
+    	2,3,0,
+    	
+    	6,7,4,
+    	4,5,6,
+
+    	8,9,10, 
+    	10,11,8
+    ];
+    return loadModel(positions, textures, index);
+}
+
+function getCurvePoint(time, points) 
+{
+    var new_points = [];
+    for (var i = 0; i < points.length-1; i++)
+    {
+        var p0 = points[i];
+        var p1 = points[i+1];
+        var p3 = [];
+        
+        var diff = [], factor = [];
+        vec2.sub(diff, p1, p0);
+        vec2.multiply(factor, diff, [time,time]);
+        vec2.add(p3, p0, factor);
+        new_points.push(p3);
+    }
+    
+    if (new_points.length == 1)
+    {
+        var grad = [];
+        vec2.sub(grad, points[1], points[0]);
+        vec2.normalize(grad, grad);
+        return {p: new_points[0], g: grad};
+    }
+    return getCurvePoint(time, new_points);
+}
+
+function calcCurveLength(points)
+{
+	var res = 20.0;
+	var step = 1.0 / res;
+	var pre_point = points[0];
+	var length = 0;
+	for (var time = step; time < 1.0; time += step)
+    {
+		var point = getCurvePoint(time, points);
+		length += vec2.distance(point.p, pre_point);
+    	pre_point = point.p;
+    }
+    return length;
+}
+
+function calcCurveMeshSegment(time, points, rad)
+{
+    var point = getCurvePoint(time, points);
+    var normal = [-point.g[1], point.g[0]];
+    vec2.multiply(normal, normal, [((rad/2.0) / asp) * osuAsp, rad/2.0]);
+    
+    var p0 = [], p1 = [];
+    vec2.add(p0, point.p, normal);
+    vec2.sub(p1, point.p, normal);
+    return [p0[0], p0[1], 0.0, 
+            p1[0], p1[1], 0.0];
+}
+
+function genCurveMesh(points, rad)
+{
+    var positions = [];
+    var textures = [];
+    var indices = [];
+    
+    var res = 20.0;
+    var step = 1.0 / res;
+    var i = 0;
+    for (var time = 0.0; time < 1.0; time += step*2.0)
+    {
+        positions = positions.concat(calcCurveMeshSegment(time, points, rad));
+        positions = positions.concat(calcCurveMeshSegment(time + step, points, rad));
+        textures = textures.concat([0.5,0.0, 0.5,1.0, 0.7,0.0, 0.7,1.0]);
+        indices = indices.concat([i+0,i+1,i+2, i+2,i+3,i+1]);
+        i += 2;
+        if (time <= 1.0 - step*2.0)
+        {
+            textures = textures.concat([0.5,0.0, 0.5,1.0, 0.7,0.0, 0.7,1.0]);
+            indices = indices.concat([i+0,i+1,i+2, i+2,i+3,i+1]);
+            i += 2;
+        }
+    }
+    
+    return {model: loadModel(positions, textures, indices), 
+    	length: calcCurveLength(points)};
+}
+
+function loadShader(vertSrc, fragSrc) 
+{
+    var shaderProgram = gl.createProgram();
+    var vertShader = compileShader(vertSrc, gl.VERTEX_SHADER);
+    var fragShader = compileShader(fragSrc, gl.FRAGMENT_SHADER);
+    
+    gl.attachShader(shaderProgram, vertShader);
+	gl.attachShader(shaderProgram, fragShader);
+	gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+    
+    gl.bindAttribLocation(shaderProgram, 0, "position");
+    gl.bindAttribLocation(shaderProgram, 1, "textureCoord");
+    
+    var defuse = gl.getUniformLocation(shaderProgram, "defuse");
+    var transform = gl.getUniformLocation(shaderProgram, "transform");
+    var colour = gl.getUniformLocation(shaderProgram, "colour");
+    var alpha = gl.getUniformLocation(shaderProgram, "alpha");
+    var crop = gl.getUniformLocation(shaderProgram, "crop");
+    gl.uniform1i(defuse, 0);
+    return {program: shaderProgram, transform: transform, 
+    	colour: colour, alpha: alpha, crop: crop};
+}
+
+function compileShader(src, type) 
+{
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+    
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('Shader compile error: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
+
+function loadSprite(file_path) 
+{
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    texture_load++;
+    
+    var image = new Image();
+    image.crossOrigin = "Anonymous";
+    image.onload = function() 
+    {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+            gl.RGBA, gl.UNSIGNED_BYTE, image);
+        
+        if (isPowerOf2(image.width) && isPowerOf2(image.height)) 
+        {
+            gl.generateMipmap(gl.TEXTURE_2D);
+        }
+        else 
+        {
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        }
+        texture_load--;
+    }
+    image.src = file_path;
+    return {texture: texture, width: image.width, height: image.height};
+}
+
+function isPowerOf2(value) 
+{
+    return (value & (value - 1)) == 0;
+}
+
+function updateDisplay() 
+{
+	display.width  = window.innerWidth;
+	display.height = window.innerHeight;
+	asp = parseFloat(display.width) / parseFloat(display.height);
+	gl.viewport(0, 0, display.width, display.height);
+	gl.clearColor(0, 0, 0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function drawModel(model, sprite) 
+{
+    // Bind texture
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, sprite.texture);
+
+    // Bind model and render
+    gl.bindVertexArray(model.vao);
+    gl.drawElements(gl.TRIANGLES, model.length, gl.UNSIGNED_SHORT, 0);
+}
+
+function drawCircle(sprite, x, y, rad, colour, alpha) 
+{
+	gl.useProgram(shader.program);
+    var screenX = (x * 2.0 - 1.0) / asp;
+    var screenY = y * 2.0 - 1.0;
+    var position = [screenX * scale * osuAsp, screenY * scale, 0];
+    var size = [rad*2.0/asp, rad*2.0, 1.0];
+    
+    var transform = mat4.create();
+    mat4.translate(transform, transform, position);
+    mat4.scale(transform, transform, size);
+    gl.uniformMatrix4fv(shader.transform, false, transform);
+    gl.uniform3f(shader.colour, colour[0], colour[1], colour[2]);
+    gl.uniform1f(shader.alpha, alpha);
+    gl.uniform1f(shader.crop, 1.0);
+    drawModel(quad, sprite);
+}
+
+function drawSlider(sprite, slider, colour, alpha)
+{
+    gl.useProgram(shader.program);
+    
+    var transform = mat4.create();
+    mat4.scale(transform, transform, [scale / asp * osuAsp * 2.0, scale * 2.0, 1.0]);
+    mat4.translate(transform, transform, [-0.5, -0.5, 0]);
+    gl.uniformMatrix4fv(shader.transform, false, transform);
+    gl.uniform3f(shader.colour, colour[0], colour[1], colour[2]);
+    gl.uniform1f(shader.alpha, alpha);
+    gl.uniform1f(shader.crop, 1.0);
+    drawModel(slider, sprite);
+}
+
+function drawSprite(sprite, x, y, width, height, alpha, crop)
+{
+	gl.useProgram(shader.program);
+	
+	var transform = mat4.create();
+	mat4.translate(transform, transform, [x, y, 0.0]);
+    mat4.scale(transform, transform, [width, height, 1.0]);
+    gl.uniformMatrix4fv(shader.transform, false, transform);
+    gl.uniform3f(shader.colour, 1, 1, 1);
+    gl.uniform1f(shader.alpha, alpha);
+    gl.uniform1f(shader.crop, crop);
+    drawModel(quad, sprite);
+}
+
+function drawBackground(sprite) 
+{
+	gl.useProgram(shader.program);
+    var fade = 0.3;
+
+    var image_asp = sprite.width / sprite.height;
+    var transform = mat4.create();
+    mat4.scale(transform, transform, [(2.0 / asp) * image_asp, 2.0, 1.0]);
+    gl.uniformMatrix4fv(shader.transform, false, transform);
+    gl.uniform3f(shader.colour, fade, fade, fade);
+    gl.uniform1f(shader.alpha, 1.0);
+    gl.uniform1f(shader.crop, 1.0);
+
+    drawModel(quad, sprite);
+}
+
---
+++ osu.js
@@ -0,0 +1,452 @@
+
+var circle, apCircle, slider_tex, slider_follow;
+var cursor, trail_tex, top_bar, health_bar;
+var combos = [];
+var hitObjects = [];
+var timing_points = [];
+var background = null;
+var startTime = 0;
+var approachRate = 7;
+var slider_mul = 1.4;
+var circle_rad = 0.15;
+var fade_time = 100;
+var currComboIndex = 0;
+var file;
+var buttons;
+var button_box;
+var drop_zone;
+
+var arTable = [
+	1800, 1680, 1560, 1440, 1320, 
+	1200, 1050, 900, 750, 600, 450
+]
+
+var mouse_x = 0, mouse_y = 0;
+var trails = [];
+var max_trails = 10;
+
+function getMouseCoords(event)
+{
+	var x = event.clientX / display.width;
+    var y = event.clientY / display.height;
+    mouse_x = x * 2.0 - 1.0;
+    mouse_y = -(y * 2.0 - 1.0);
+    
+    trails.push([mouse_x, mouse_y]);
+    if (trails.length > max_trails)
+    	trails.shift();
+}
+
+function initOsu() 
+{
+	initGameEngine();
+    circle = loadSprite("Skin/hitcircle.png");
+    apCircle = loadSprite("Skin/approachcircle.png");
+    slider_tex = loadSprite("slider.png");
+    slider_follow = loadSprite("Skin/sliderfollowcircle.png");
+    cursor = loadSprite("Skin/cursor.png");
+    trail_tex = loadSprite("Skin/cursor-smoke.png");
+    top_bar = loadSprite("Skin/scorebar-bg.png");
+    health_bar = loadSprite("Skin/scorebar-colour.png");
+    buttons = document.getElementById("buttons");
+    button_box = document.getElementById("button_box");
+    drop_zone = document.getElementById("drop_zone");
+    
+    setInterval(update, 1000.0/fps);
+}
+
+function fileDropped(event)
+{
+	console.log("file dropped");
+	event.preventDefault();
+	file = event.dataTransfer.items[0].getAsFile();
+	hitObjects = [];
+
+	JSZip.loadAsync(file).then(function(zip) 
+	{
+		while (button_box.firstChild) {
+			button_box.removeChild(button_box.firstChild);
+		}
+		zip.forEach(function (relativePath, zipEntry) 
+		{
+			console.log("Found file: " + zipEntry.name);
+			if (zipEntry.name.endsWith('.osu'))
+			{
+				var btn = document.createElement("button");
+				btn.innerHTML = zipEntry.name;
+				btn.onclick = function()
+				{
+					loadMap(zipEntry.name);
+				}
+				button_box.appendChild(btn);
+			}
+		});
+		buttons.style = "margin-top:0px;";
+	}, function (e) {});
+}
+
+function dragOverHandler(event) 
+{
+	event.preventDefault();
+}
+
+function loadMap(map)
+{
+	combos = [];
+	hitObjects = [];
+	timing_points = [];
+	closeBox();
+
+	JSZip.loadAsync(file).then(function(zip) 
+	{
+		return zip.file(map).async("text");
+	}).then(function(txt) 
+	{
+		var lines = txt.split('\n');
+		decodeMap(lines);
+	});
+}
+
+function closeBox()
+{
+	buttons.style = "margin-top:50%;";
+}
+
+function decodeMap(lines) 
+{
+    currComboIndex = 0;
+	var mode = "";
+	for(var i = 0; i < lines.length; i++) 
+    {
+		var line = lines[i];
+        if (line.trim() == "") 
+        {
+            continue;
+        }
+        
+		if (line.startsWith('[')) 
+        {
+			mode = line.substring(1, line.length-2);
+			console.log("Mode: " + mode);
+		}
+		else 
+        {
+			switch(mode)
+			{
+				case "Difficulty": decodeDiffuculty(line); break;
+				case "Events": decodeEvents(line); break;
+				case "TimingPoints": decodeTimings(line); break;
+                case "Colours": decodeColours(line); break;
+				case "HitObjects": decodeHit(line); break;
+			}
+		}
+	}
+	
+	closeBox();
+	display.style = "display:block;opacity:1";
+	drop_zone.style = "display:none";
+	startTime = Date.now();
+}
+
+function decodeDiffuculty(line) 
+{
+	var data = line.split(":");
+	if (data[0] == "ApproachRate") 
+    {
+		approachRate = parseInt(data[1]);
+		console.log("Approach Rate: " + approachRate);
+	}
+	else if (data[0] == "SliderMultiplier")
+	{
+		slider_mul = parseFloat(data[1]);
+		console.log("Slider Multiplier: " + slider_mul);
+	}
+}
+
+function decodeEvents(line)
+{
+	var data = line.split(",");
+	if (data.length == 5) 
+    {
+		var fileName = data[2].substring(1, data[2].length - 1);
+		loadImageFromZip(fileName);
+		background = loadSprite("Song/" + fileName);
+		console.log("Background: " + fileName);
+	}
+}
+
+function loadImageFromZip(image_path)
+{
+	var path_len = image_path.length;
+	var type = image_path.substring(path_len - 3, path_len);
+	JSZip.loadAsync(file).then(function(zip) 
+	{
+		return zip.file(image_path).async("base64");
+	}).then(function(txt) 
+	{
+		var img_url = "data:image/" + type + ";base64," + txt;
+		background = loadSprite(img_url);
+	});
+}
+
+var last_positive_beat = 0;
+
+function decodeTimings(line)
+{
+	var data = line.split(",");
+	var tp = new Object();
+	
+	tp.offset = timing_points.length == 0 ? 0 : parseInt(data[0]);
+	tp.mpb = parseFloat(data[1]);
+	tp.meter = parseFloat(data[2]);
+	timing_points.push(tp);
+	console.log("Timing point at " + tp.offset);
+	
+	if (tp.mpb >= 0)
+	{
+		last_positive_beat = tp.mpb;
+	}
+	else
+	{
+		tp.mpb = last_positive_beat * (-tp.mpb / 100.0);
+	}
+}
+
+function decodeColours(line) 
+{
+    var data = line.split(":");
+    var colourData = data[1].split(",");
+    
+    var colour = [];
+    for (var i = 0; i < colourData.length; i++) 
+    {
+        colour.push(parseInt(colourData[i]) / 255.0);
+    }
+    
+    var name = data[0];
+    var id = parseInt(name.substring(5, name.length));
+    combos.push(colour);
+    console.log(name + ": " + colour);
+}
+
+function findTimingPoint(time)
+{
+	var max_point = {offset:0};
+	for (var i = 0; i < timing_points.length; i++)
+	{
+		var tp = timing_points[i];
+		if (tp.offset < time && max_point.offset < tp.offset)
+			max_point = tp;
+	}
+	return max_point;
+}
+
+function decodeHit(line) 
+{
+	var hitObject = new Object();
+	
+	var data = line.split(",");
+	hitObject.x = parseFloat(data[0]) / 512.0;
+	hitObject.y = 1.0 - (parseFloat(data[1]) / 384.0);
+	hitObject.time = parseInt(data[2]);
+	hitObject.type = parseInt(data[3]);
+	hitObject.tp = findTimingPoint(hitObject.time);
+	hitObject.slider_length = 0;
+	
+	if ((hitObject.type & 0x3) == 2) 
+    {
+		var sliderInfo = data[5];
+		var points = sliderInfo.split("|");
+		
+		hitObject.points = [];
+		for (var i = 1; i < points.length; i++) 
+        {
+			var point = points[i].split(":");
+			var x = parseFloat(point[0]) / 512.0;
+			var y = 1.0 - (parseFloat(point[1]) / 384.0);
+			hitObject.points.push([x, y]);
+		}
+		
+		hitObject.sliderType = points[0];
+        hitObject.sliders = genSliderMesh(hitObject);
+        hitObject.pixel_length = parseFloat(data[7]);
+        hitObject.slider_length = hitObject.pixel_length / 
+        	(100.0 * slider_mul) * hitObject.tp.mpb;
+	}
+	
+    if ((hitObject.type & 0x4) == 0x4) 
+    {
+    	var skip = (hitObject.type >> 3) & 0x3;
+    	currComboIndex += 1 + skip;
+    	currComboIndex = currComboIndex % combos.length;
+    }
+	hitObject.colour = combos[currComboIndex];
+    
+	hitObjects.push(hitObject);
+}
+
+function update() 
+{
+	updateDisplay();
+	if (background != null) 
+		drawBackground(background);
+	
+	var currTime = Date.now() - startTime;
+	var delay = arTable[approachRate];
+	for (var i = 0; i < hitObjects.length; i++) 
+    {
+		var hit = hitObjects[i];
+		var scale = 1.8;
+		if (currTime < hit.time + hit.slider_length + fade_time && currTime > hit.time - delay) 
+        {
+        	var alpha = calcAlpha(hit, currTime, delay);
+        
+			var x = hit.x;
+			var y = hit.y;
+			if ((hit.type & 0x3) == 1) 
+				drawCircle(circle, x, y, circle_rad, hit.colour, alpha);
+			else if ((hit.type & 0x3) == 2) 
+				drawSliderHit(hit, currTime, alpha);
+			drawApCircle(hit, currTime, delay, alpha);
+		}
+	}
+	
+	drawUI();
+	drawCursor();
+}
+
+var health = 1.0;
+var time = 0;
+function drawUI()
+{
+	var scale = 0.2;
+	var bar_asp = top_bar.width / top_bar.height;
+	var width = (scale * bar_asp) / asp;
+	
+	health = Math.sin(time);
+	time += 0.01;
+	drawSprite(top_bar, -1 + width/2, 1 - scale/2, width, scale, 1.0, 1.0);
+	drawSprite(health_bar, -1 + width/2 + 0.01/asp, 1 - scale/2 - 0.04, 
+		width, scale, 1.0, health);
+}
+
+function drawCursor()
+{
+	// Draw trails
+    var cursor_size = 0.15;
+	for (var i = 0; i < trails.length; i++)
+	{
+		var pos = trails[i];
+		drawSprite(trail_tex, pos[0], pos[1], 
+			cursor_size / asp, cursor_size, i / trails.length, 1.0);
+	}
+	
+	// Draw cursor
+    var x = mouse_x;
+    var y = mouse_y;
+    drawSprite(cursor, x, y, (cursor_size*2) / asp, cursor_size*2, 1.0, 1.0);
+    
+    trails.push([mouse_x, mouse_y]);
+    if (trails.length > max_trails)
+    	trails.shift();
+}
+
+function calcAlpha(hit, currTime, delay)
+{
+	var alpha = 1.0;
+	if (currTime > hit.time + hit.slider_length)
+	{
+		var factor = (currTime - (hit.time + hit.slider_length)) / fade_time;
+		alpha = 1.0 - factor;
+	}
+	if (currTime < hit.time - delay + fade_time)
+	{
+		var factor = ((hit.time - delay + fade_time) - currTime) / fade_time;
+		alpha = 1.0 - factor;
+	}
+	return alpha;
+}
+
+function drawApCircle(hit, currTime, delay, alpha)
+{
+	var x = hit.x;
+	var y = hit.y;
+	if (currTime < hit.time)
+	{
+		var offset = 1.0 - ((currTime - (hit.time - delay)) / delay);
+		drawCircle(apCircle, x, y, circle_rad + (offset * 0.2), hit.colour, alpha);
+	}
+}
+
+function drawSliderHit(hit, currTime, alpha)
+{
+	var total_length = 0.0;
+	for (var i = 0; i < hit.sliders.length; i++)
+	{
+		var slider = hit.sliders[i].slider;
+		total_length += slider.length;
+	}
+
+	var offset = 0.0;
+	for (var i = 0; i < hit.sliders.length; i++)
+    {
+    	var slider_info = hit.sliders[i];
+    	var slider = slider_info.slider;
+		drawSlider(slider_tex, slider.model, hit.colour, alpha);
+		
+		// Draw follow circle
+        if (currTime > hit.time)
+        {
+        	var rel_length = slider.length / total_length;
+        	var factor = (currTime - hit.time) / hit.slider_length;
+        	if (factor > offset && factor < offset + rel_length)
+        	{
+        		factor = (factor - offset) / rel_length;
+        		var point = getCurvePoint(factor, slider_info.points);
+        		drawCircle(slider_follow, point.p[0], point.p[1], circle_rad, hit.colour, 1.0);
+        	}
+        	offset += rel_length;
+        }
+    }
+}
+
+function genSliderMesh(hit) 
+{
+	var start = [hit.x, hit.y];
+	var end = hit.points[hit.points.length-1];
+	var slider_rad = circle_rad + 0.05;
+    
+	if(hit.sliderType == "P" || hit.sliderType == "L")
+    {
+    	var points = [start].concat(hit.points);
+		return [{slider: genCurveMesh(points, slider_rad), points: points}];
+	}
+	else if(hit.sliderType == "B")
+    {
+        var sliders = [];
+		var currPoints = [start];
+		for (var i = 0; i < hit.points.length; i++) 
+        {
+			var point = hit.points[i];
+			if (currPoints.length > 0)
+            {
+				var lastPoint = currPoints[currPoints.length-1];
+				if (point[0] == lastPoint[0] && point[1] == lastPoint[1])
+                {
+					sliders.push({slider: genCurveMesh(currPoints, slider_rad), 
+						points: currPoints});
+					currPoints = [];
+				}
+			}
+			currPoints.push(point);
+		}
+		sliders.push({slider: genCurveMesh(currPoints, slider_rad), 
+			points: currPoints});
+        return sliders;
+	}
+}
+
+// 165,124, 101,145, 59,79, 59,79, 134,8, 197,55, 190,96, 273,92, 273,92, 223,139, 244,181, 275,211, 271,258, 271,258, 274,196, 331,171, 331,171, 329,186, 317,189
+
+// ABC CDEFG G
+
